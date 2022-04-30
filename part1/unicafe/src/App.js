8a2cb96edf40d1670e4a116147c73ceed40802bb
Jiezhi Winston Zuo-yu <zywinston@gmail.com>
2022-04-30 18:47:15 -0400

    Update App.js

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,3 +1,4 @@
+import { buildTimeValue } from '@testing-library/user-event/dist/utils'
 import { useState } from 'react'
 
 const App = () => {
@@ -5,7 +6,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  console.log(good)
+  console.log(good, neutral, bad, good + neutral + bad);
+
+  //sets state to value of given function set
   const setValue = (value, set) => {
     set(value + 1)
   }
@@ -14,23 +17,39 @@ const App = () => {
     <button onClick={props.handleClick}>
       {props.text}
     </button>
-  )
+  );
 
-  return ( 
+  const StatisticLine = (props) => {
+    return (
+      <div>{props.text} {props.value}{props.extratext}</div>
+    );  
+  }
+
+  const Statistics = (props) => {
+    if (props.total == 0)
+      return <div>No feedback given</div>
+    return (
     <div>
-      <p><strong>give feedback </strong></p>
+      <StatisticLine text = "good" value = {props.good}/>
+      <StatisticLine text = "neutral" value = {props.neutral}/>
+      <StatisticLine text = "bad" value = {props.bad}/>
+      <StatisticLine text = "all" value = {props.total}/>
+      <StatisticLine text = "average" value = {(props.good + props.neutral + props.bad) / 3}/>
+      <StatisticLine text = "positive" value = {props.good/(props.good + props.neutral + props.bad) * 100} extratext = "%"/>
+    </div>
+    );
+  }
+
+  return (
+    <div>
+      <p><b><font size = "+3">give feedback</font></b></p>
       <Button handleClick = {() => setValue(good, setGood)} text = "good"/>
       <Button handleClick = {() => setValue(neutral, setNeutral)} text = "neutral"/>
       <Button handleClick = {() => setValue(bad, setBad)} text = "bad"/>
-      <p><strong>statistics</strong></p>
-      <p>
-          good {good}<br/>
-          neutral {neutral}<br/>
-          bad {bad}<br/>
-      </p>
-
+      <p><b><font size = "+3">statistics</font></b></p>
+      <Statistics good = {good} bad = {bad} neutral = {neutral} total = {good + neutral + bad}/>
     </div>
-  )
+  );
 }
 
 export default App
